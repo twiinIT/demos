0a326b33a274ffd869babc5ac1294ed52ca12134
Guy De Spiegeleer <guy.de-spiegeleer@m4x.org>
2022-01-23 18:18:54 +0100

    unit in fluid port

--- demos/cpu/cpu/ports/fluid.py
+++ demos/cpu/cpu/ports/fluid.py
@@ -4,7 +4,7 @@ from cosapp.ports import Port
 class Fluid(Port):
     def setup(self):
 
-        self.add_variable("mass_flow", desc="Mass flow")
-        self.add_variable("p", desc="Pressure")
-        self.add_variable("T", desc="Temperature")
-        self.add_variable("cp", 1004.0, desc="Heat capacity at constant pressure")
+        self.add_variable("mass_flow", unit="kg/s", desc="Mass flow")
+        self.add_variable("p", unit="pa", desc="Pressure")
+        self.add_variable("T", unit="K", desc="Temperature")
+        self.add_variable("cp", 1004.0, unit="J/K/kg", desc="Heat capacity at constant pressure")
