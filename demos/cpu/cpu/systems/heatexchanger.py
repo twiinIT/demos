3f9d2ad4bd530103291d4a304e5f4f38a1801691
Guy De Spiegeleer <guy.de-spiegeleer@m4x.org>
2022-01-23 18:26:11 +0100

    heat conductivity constant

--- demos/cpu/cpu/systems/heatexchanger.py
+++ demos/cpu/cpu/systems/heatexchanger.py
@@ -14,17 +14,16 @@ class HeatExchanger(System):
         self.add_inward("T", 0.0, unit="K", desc="Metal temperature")
         self.add_inward("surface", 0.01, unit="m**2", desc="Exchanger surface")
         self.add_inward("cp", 1004.0, unit="J/K/kg", desc="Heat capacity at constant pressure")
+        self.add_inward("h", 310.0, unit="W/K/m**2", desc="Heat conductivity")
 
         # outputs
         self.add_output(Fluid, "fl_out")
-        self.add_outward("h", 20.0, unit="W/K/m**2", desc="Heat conductivity")
         self.add_outward("heat_flow", 0.0, unit="W", desc="Exchanger-to-air heat flow")
 
         # transient
         self.add_transient("dH", der="heat_flow", desc="Enthalpy delta")
 
     def compute(self):
-        self.h = 10.0 + 300.0 * max(self.fl_in.mass_flow / 1.0, 1.0)
         self.heat_flow = self.h * (self.T - self.fl_in.T) * self.surface
 
         self.fl_out.mass_flow = self.fl_in.mass_flow
