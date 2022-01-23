73cfcd87e1f6e5d490d6395cc4355e866010a7c3
Guy De Spiegeleer <guy.de-spiegeleer@m4x.org>
2022-01-23 18:04:41 +0100

    unit added in CPU

--- demos/cpu/cpu/systems/cpu.py
+++ demos/cpu/cpu/systems/cpu.py
@@ -8,8 +8,8 @@ class CPU(System):
     def setup(self):
         # inputs
         self.add_inward("usage", 20.0, desc="Usage percentage")
-        self.add_inward("tdp", 105.0, "W", desc="Thermal Design Power")
-        self.add_inward("heat_flow", 0.0, desc="Exit thermal flow")
+        self.add_inward("tdp", 105.0, unit="W", desc="Thermal Design Power")
+        self.add_inward("heat_flow", 0.0, unit="W", desc="Exit thermal flow")
         self.add_inward("heat_capacity", 5.0, "J/K", desc="CPU heat capacity")
         self.add_inward("T", 273.15, unit="K", desc="Metal temperature")
 
