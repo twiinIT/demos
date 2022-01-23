d49f811120a24653cdb8fed0a318eedd01f36a4c
Guy De Spiegeleer <guy.de-spiegeleer@m4x.org>
2022-01-23 18:01:08 +0100

    comments added

--- demos/cpu/cpu/systems/fancontroller.py
+++ demos/cpu/cpu/systems/fancontroller.py
@@ -2,7 +2,11 @@ from cosapp.systems import System
 
 
 class FanController(System):
+    """ 
+    Define tension for fan from the level of CPU temperature
+    """
     def setup(self):
+        # inputs
         self.add_inward("T", unit="K", desc="Temperature")
         self.add_inward("low_threshold", 303.15, desc="Low to medium temperature threshold")
         self.add_inward("high_threshold", 333.15, desc="Medium to high temperature threshold")
@@ -10,6 +14,7 @@ class FanController(System):
         self.add_inward("medium_tension", 6.0, desc="Output medium tension")
         self.add_inward("max_tension", 12.0, desc="Output max tension")
 
+        # outputs
         self.add_outward("tension", 0.0, desc="Output tension")
 
     def compute(self):
--- demos/cpu/cpu/systems/heatexchanger.py
+++ demos/cpu/cpu/systems/heatexchanger.py
@@ -5,15 +5,21 @@ from cpu.ports.fluid import Fluid
 
 
 class HeatExchanger(System):
+    """ 
+    Compute heatflux extracted by air flow
+    """
     def setup(self):
+        # inputs
         self.add_input(Fluid, "fl_in")
         self.add_inward("T", 0.0, unit="K", desc="Metal temperature")
         self.add_inward("h", 20.0, desc="Heat conductivity")
         self.add_inward("surface", 0.01, unit="m**2", desc="Exchanger surface")
 
+        # outputs
         self.add_output(Fluid, "fl_out")
         self.add_outward("heat_flow", 0.0, desc="Exchanger-to-air heat flow")
 
+        # transient
         self.add_transient("dH", der="heat_flow", desc="Enthalpy delta")
 
     def compute(self):
