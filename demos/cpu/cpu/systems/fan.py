171f8c1818c24a848be8ee70bc27d6df146d7c7f
Guy De Spiegeleer <guy.de-spiegeleer@m4x.org>
2022-01-23 18:07:26 +0100

    units in fan

--- demos/cpu/cpu/systems/fan.py
+++ demos/cpu/cpu/systems/fan.py
@@ -10,9 +10,9 @@ class Fan(System):
     def setup(self):
         # inputs
         self.add_input(Fluid, "fl_in")
-        self.add_inward("tension", 0.0, desc="Input tension")
-        self.add_inward("design_tension", 12.0, desc="Fan input tension at design point")
-        self.add_inward("cfm", 1.0, desc="Cubic feet per minute")
+        self.add_inward("tension", 0.0, unit="V", desc="Input tension")
+        self.add_inward("design_tension", 12.0, unit="V", desc="Fan input tension at design point")
+        self.add_inward("cfm", 1.0, unit="ft**3/min", desc="Cubic feet per minute")
 
         # outputs
         self.add_output(Fluid, "fl_out")
