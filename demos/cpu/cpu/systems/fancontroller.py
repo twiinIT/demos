d65ca2db487e253723d0f3c2508a1bcd433cf56a
Guy De Spiegeleer <guy.de-spiegeleer@m4x.org>
2022-01-27 17:57:51 +0100

    Updates proposals for clarity (#1)
    * comments added
    * bug h in HeatExchanger is outward not inward
    * unit added in CPU
    * units in fan
    * units in FanController nd HeatExchanger
    * unit in fluid port
    * p removed from port fluid
    * cp air in heatexchanger and remodev from port
    * heat conductivity constant
    * fl_in changed into T_air in Fan
    * notebook updated
    * cleaning notebook
    * update few comments
    
    Co-authored-by: Adrien DELSALLE <ad.delsalle@gmail.com>

--- demos/cpu/cpu/systems/fancontroller.py
+++ demos/cpu/cpu/systems/fancontroller.py
@@ -2,15 +2,20 @@ from cosapp.systems import System
 
 
 class FanController(System):
+    """ 
+    Define tension for fan from the level of CPU temperature
+    """
     def setup(self):
+        # inputs
         self.add_inward("T", unit="K", desc="Temperature")
-        self.add_inward("low_threshold", 303.15, desc="Low to medium temperature threshold")
-        self.add_inward("high_threshold", 333.15, desc="Medium to high temperature threshold")
-        self.add_inward("low_tension", 0.0, desc="Output low tension")
-        self.add_inward("medium_tension", 6.0, desc="Output medium tension")
-        self.add_inward("max_tension", 12.0, desc="Output max tension")
+        self.add_inward("low_threshold", 303.15, unit="K", desc="Low to medium temperature threshold")
+        self.add_inward("high_threshold", 333.15, unit="K", desc="Medium to high temperature threshold")
+        self.add_inward("low_tension", 0.0, unit="V", desc="Output low tension")
+        self.add_inward("medium_tension", 6.0, unit="V", desc="Output medium tension")
+        self.add_inward("max_tension", 12.0, unit="V", desc="Output max tension")
 
-        self.add_outward("tension", 0.0, desc="Output tension")
+        # outputs
+        self.add_outward("tension", 0.0, unit="V", desc="Output tension")
 
     def compute(self):
         if self.T <= self.low_threshold:
--- demos/cpu/cpu/systems/heatexchanger.py
+++ demos/cpu/cpu/systems/heatexchanger.py
@@ -5,21 +5,26 @@ from cpu.ports.fluid import Fluid
 
 
 class HeatExchanger(System):
+    """ 
+    Compute heatflux extracted by air flow
+    """
     def setup(self):
+        # inputs
         self.add_input(Fluid, "fl_in")
         self.add_inward("T", 0.0, unit="K", desc="Metal temperature")
-        self.add_inward("h", 20.0, desc="Heat conductivity")
         self.add_inward("surface", 0.01, unit="m**2", desc="Exchanger surface")
+        self.add_inward("cp", 1004.0, unit="J/K/kg", desc="Heat capacity at constant pressure")
+        self.add_inward("h", 310.0, unit="W/K/m**2", desc="Heat conductivity")
 
+        # outputs
         self.add_output(Fluid, "fl_out")
-        self.add_outward("heat_flow", 0.0, desc="Exchanger-to-air heat flow")
+        self.add_outward("heat_flow", 0.0, unit="W", desc="Exchanger-to-air heat flow")
 
+        # transient
         self.add_transient("dH", der="heat_flow", desc="Enthalpy delta")
 
     def compute(self):
-        self.h = 10.0 + 300.0 * max(self.fl_in.mass_flow / 1.0, 1.0)
         self.heat_flow = self.h * (self.T - self.fl_in.T) * self.surface
 
         self.fl_out.mass_flow = self.fl_in.mass_flow
-        self.fl_out.T = self.fl_in.T + self.dH / self.fl_in.cp
-        self.fl_out.p = self.fl_in.p
+        self.fl_out.T = self.fl_in.T + self.dH / self.cp
