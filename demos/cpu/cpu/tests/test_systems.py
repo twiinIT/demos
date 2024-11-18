0e3fa1c0afe00ac74806a52095dfd9f665bcba76
Guy De Spiegeleer <guy.de-spiegeleer@twiinit.com>
2024-11-18 16:02:48 +0100

    add tests

---
+++ demos/cpu/cpu/tests/test_systems.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2024, twiinIT
+# SPDX-License-Identifier: BSD-3-Clause
+
+import pytest
+from cpu.systems import CPU, CPUSystem, Fan, FanController, HeatExchanger
+
+cls_list = [CPU, CPUSystem, Fan, FanController, HeatExchanger]
+
+@pytest.mark.parametrize("cls", cls_list)
+class TestSystems:
+    """Define generic tests for cosapp systems."""
+
+    def test_setup(self, cls):
+        print("Class name:", cls.__name__)
+        cls("sys")
+        assert True
+
+    def test_once(self, cls):
+        print("Class name:", cls.__name__)
+        sys = cls("sys")
+        sys.run_once()
+
+        assert True
