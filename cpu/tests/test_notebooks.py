162336b0fa23a974970d36210abf5b707e6927fe
Guy De Spiegeleer <guy.de-spiegeleer@twiinit.com>
2024-11-28 21:08:15 +0100

    pre-commit

--- cpu/tests/test_notebooks.py
+++ cpu/tests/test_notebooks.py
@@ -1,44 +1,44 @@
 # Copyright (C) 2024, twiinIT
 # SPDX-License-Identifier: BSD-3-Clause
 
-import os
 import glob
+import os
 
-import pytest
 import nbformat
-
+import pytest
 from nbconvert.preprocessors import ExecutePreprocessor
 
 NOTEBOOKS_DIR = "cpu/notebooks"
 
+
 def list_notebooks(directory):
-    return [
-        os.path.join(directory, f)
-        for f in os.listdir(directory)
-        if f.endswith(".ipynb")
-    ]
+    return [os.path.join(directory, f) for f in os.listdir(directory) if f.endswith(".ipynb")]
+
 
 list_nb = list_notebooks(NOTEBOOKS_DIR)
-list_nb = ["cpu/CPU_demos.ipynb", 
-           "cpu/notebooks/steady-state_design.ipynb", 
-           "cpu/notebooks/transient_simulation.ipynb", 
-           "cpu/notebooks/generate_nominal_operation_data.ipynb", 
-           "cpu/notebooks/generate_dysfunctional_operation_data.ipynb",
-           "cpu/notebooks/same_operating_conditions_simulation.ipynb",
-           "cpu/notebooks/ai_dataset_for_event_detection.ipynb",
-           "cpu/notebooks/ai_training_for_event_detection.ipynb",
-           "cpu/notebooks/spot_event_using_ai.ipynb",
-           "cpu/notebooks/calibration_transient_basics.ipynb",
-           "cpu/notebooks/calibration_broken_wo_event.ipynb",
-           "cpu/notebooks/calibration_broken_with_event.ipynb",
-           "cpu/notebooks/doe.ipynb",
-           "cpu/notebooks/montecarlo.ipynb",
-           "cpu/notebooks/solver_debugging.ipynb",
+list_nb = [
+    "cpu/CPU_demos.ipynb",
+    "cpu/notebooks/steady-state_design.ipynb",
+    "cpu/notebooks/transient_simulation.ipynb",
+    "cpu/notebooks/generate_nominal_operation_data.ipynb",
+    "cpu/notebooks/generate_dysfunctional_operation_data.ipynb",
+    "cpu/notebooks/same_operating_conditions_simulation.ipynb",
+    "cpu/notebooks/ai_dataset_for_event_detection.ipynb",
+    "cpu/notebooks/ai_training_for_event_detection.ipynb",
+    "cpu/notebooks/spot_event_using_ai.ipynb",
+    "cpu/notebooks/calibration_transient_basics.ipynb",
+    "cpu/notebooks/calibration_broken_wo_event.ipynb",
+    "cpu/notebooks/calibration_broken_with_event.ipynb",
+    "cpu/notebooks/doe.ipynb",
+    "cpu/notebooks/montecarlo.ipynb",
+    "cpu/notebooks/solver_debugging.ipynb",
 ]
+
 
 @pytest.fixture(scope="session")
 def execute_preprocessor():
     return ExecutePreprocessor(timeout=600)
+
 
 @pytest.mark.notebooks
 @pytest.mark.parametrize("notebook_path", list_nb)
@@ -48,8 +48,7 @@ def test_notebook_execution(execute_preprocessor, notebook_path):
 
     try:
         execute_preprocessor.preprocess(
-            notebook,
-            {"metadata": {"path": os.path.dirname(notebook_path)}}
+            notebook, {"metadata": {"path": os.path.dirname(notebook_path)}}
         )
     except Exception as e:
         pytest.fail(f"Notebook execution failed for {notebook_path}: {e}")
