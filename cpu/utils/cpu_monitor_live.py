b8eb9221d6b56e60b8d653fe32976e18f79ec9de
Ahmed_mahamat <korommht@gmail.com>
2025-04-18 16:48:35 +0200

    Add cpu monitoring (#25)
    * add cpu monitoring
    
    * pre-commit
    
    * pre-commit
    
    * demo notebook monitoring
    
    * adaptation to linux
    
    * adaptation to linux v2
    
    * adaptation to linux v3
    
    * review_1 changes
    
    * review_2 changes

---
+++ cpu/utils/cpu_monitor_live.py
@@ -0,0 +1,85 @@
+import platform
+import subprocess
+import time
+from multiprocessing import Process, Queue
+
+import psutil
+
+
+def spin(duration: float):
+    """Spin CPU."""
+    t0 = time.time()
+    while time.time() < t0 + duration:
+        pass
+
+
+def run_spinners(cpu_count: int, duration: float):
+    """Apply spin process for each selected CPU processor."""
+    processes = [Process(target=spin, args=(duration,)) for _ in range(cpu_count)]
+    for process in processes:
+        process.start()
+    for process in processes:
+        process.join()
+
+
+def get_cpu_info(queue: Queue):
+    """Assess the platform and collect parameters from running CPU processors."""
+    is_mac = platform.system() == "Darwin"
+    is_linux = platform.system() == "Linux"
+
+    t0 = time.time()
+    while time.time() - t0 < 90:
+        usage = psutil.cpu_percent(interval=1)
+
+        if is_mac:
+            # Temperature via istats
+            try:
+                result = subprocess.run(
+                    ["istats", "cpu", "--value-only"], capture_output=True, text=True
+                )
+                output = result.stdout.strip().replace("°C", "").strip()
+                temp_val = float(output)
+            except ValueError:
+                temp_val = None
+
+            # Get fan RPMs via istats
+            try:
+                result_fan = subprocess.run(
+                    ["istats", "fan", "--value-only"], capture_output=True, text=True
+                )
+                fan_output_lines = result_fan.stdout.strip().splitlines()
+                fan_rpms = int(fan_output_lines[1].replace("RPM", "").strip())
+            except ValueError:
+                fan_rpms = None
+
+        elif is_linux:
+            # Temperature via ps_utils
+            try:
+                temps = psutil.sensors_temperatures()
+                temp_val = temps["coretemp"][0].current
+            except Exception:
+                temp_val = None
+
+            # Fan RPMs via ps_utils
+            try:
+                fan_rpms = psutil.sensors_fans().popitem()[1][0].current
+            except Exception:
+                fan_rpms = None
+
+        elapsed = round(time.time() - t0, 3)
+        queue.put((elapsed, usage, temp_val, fan_rpms))
+
+    queue.put("DONE")
+
+
+def run_cpu_monitor(queue: Queue):
+    """Run CPU info collection process."""
+    process = Process(target=get_cpu_info, args=(queue,))
+    process.start()
+    return process
+
+
+def run_pipeline(pipeline):
+    """Run each step of pipeline."""
+    for func, args in pipeline:
+        func(*args)
---
+++ cpu/utils/monitor_simulation.py
@@ -0,0 +1,50 @@
+from cosapp.drivers import NonLinearSolver
+
+from cpu.systems import CPUSystem
+
+
+def run_simulation(queue):
+    """Run simulation at each time step collecting live CPU data."""
+    cpu = CPUSystem("cpu")
+    cpu.add_driver(NonLinearSolver("solver", max_iter=10, factor=1.0, tol=1e-6))
+    cpu["exchanger.h_adder"] = 150
+    cpu["cpu.heat_capacity"] = 100
+
+    simulation_results = []
+    i = 0
+    simulated_T = 0.0
+    while True:
+        data = queue.get()
+        if data == "DONE":
+            break
+
+        # prepare and run simu
+        t, usage, measured_T, fan_rpms = data
+        cpu["cpu.usage"] = usage
+        cpu["T_cpu"] = simulated_T if i != 0 else measured_T
+        cpu.run_drivers()
+        # get results
+
+        simulated_T = cpu["cpu.next_T"]
+        cpu["cpu.T"] = simulated_T
+        i += 1
+
+        print(
+            f"[SIM] t={t:.1f}s | \
+              usage={usage}% | \
+              T_sim={simulated_T:.2f}°C | \
+              T_meas={measured_T} | \
+              Fan_rpm={fan_rpms}"
+        )
+
+        simulation_results.append(
+            {
+                "time": t,
+                "cpu.usage": usage,
+                "T_cpu_simulated": simulated_T,
+                "T_cpu_measured": measured_T,
+                "Fan_rpm_1": fan_rpms,
+            }
+        )
+
+    queue.put(simulation_results)
